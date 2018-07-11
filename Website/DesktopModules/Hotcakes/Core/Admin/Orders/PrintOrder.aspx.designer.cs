8d5162a6694224fca751c33cb9d8dce3c1eb163b
Ketan Garala <ketangarala@gmail.com>
2018-07-11 20:20:48 +0530

    #99 SI: Printing multiple orders from order manager no longer includes built-in page breaks between orders

--- Website/DesktopModules/Hotcakes/Core/Admin/Orders/PrintOrder.aspx.designer.cs
+++ Website/DesktopModules/Hotcakes/Core/Admin/Orders/PrintOrder.aspx.designer.cs
@@ -49,13 +49,13 @@ namespace Hotcakes.Modules.Core.Admin.Orders {
         protected global::System.Web.UI.WebControls.ImageButton btnGenerate;
         
         /// <summary>
-        /// DataList1 control.
+        /// rpOrder control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DataList DataList1;
+        protected global::System.Web.UI.WebControls.Repeater rpOrder;
         
         /// <summary>
         /// btnContinue2 control.
