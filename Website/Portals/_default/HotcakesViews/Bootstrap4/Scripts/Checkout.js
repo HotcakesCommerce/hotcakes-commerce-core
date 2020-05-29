c2f82bb2635a848152822be3e6b68ce6c0b09ffd
Will Strohl <will@upendoventures.com>
2020-05-29 15:13:45 -0700

    BS4 conversion updates for receipt views

--- Website/Portals/_default/HotcakesViews/Bootstrap4/Scripts/Checkout.js
+++ Website/Portals/_default/HotcakesViews/Bootstrap4/Scripts/Checkout.js
@@ -613,7 +613,6 @@
                 $('#hcDeliverySection').ajaxLoader('stop');
                 $('#hcShippingRates').html(data.rates);
                 $('#hcShippingRates').show().removeClass("d-none");
-                $('#hcShippingRates input').attr("tabindex", "300");
                 BindShippingRadioButtons();
                 ApplyCurrentShippingRate();
             },
