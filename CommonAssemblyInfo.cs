473946044ef6d028323735a98e5770de953b5de4
Will Strohl <will@upendoventures.com>
2020-05-31 11:55:37 -0700

    Release notes & versioning

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,15 +3,15 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Hotcakes Commerce, LLC")]
+[assembly: AssemblyCompany("Upendo Ventures, LLC")]
 [assembly: AssemblyProduct("Hotcakes Commerce")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2020 Upendo Ventures, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.02.03")]
+[assembly: AssemblyVersion("3.03.00")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
