0a9a54b86d8609d5d04c523ea6b25a79e149b994
Will Strohl <will@upendoventures.com>
2020-06-01 18:03:52 -0700

    Final packaging updates prior to release

--- Libraries/Hotcakes.Commerce/Catalog/Option.cs
+++ Libraries/Hotcakes.Commerce/Catalog/Option.cs
@@ -190,6 +190,7 @@ namespace Hotcakes.Commerce.Catalog
         ///     (optional) When specified, this value will be included as a prefix to the Name attribute of the
         ///     rendered items.
         /// </param>
+        /// <param name="className">When specified, the given class name will be appended to the controls rendered to the view.</param>
         /// <returns>
         ///     An HTML-friendly string representation of the Option, based upon its respective OptionType, with default
         ///     selections made.
