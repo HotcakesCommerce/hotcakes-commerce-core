f166643e97cfabc7804373fdeff2e3b74470579f
Will Strohl <will@upendoventures.com>
2020-10-24 12:01:46 -0700

    Properly updated DevSample DLLs to 3.5.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.04.01")]
+[assembly: AssemblyVersion("3.05.00")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
