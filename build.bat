317fa1f2cb781275e7430887993de52100aec9ce
Will Strohl <will@upendoventures.com>
2021-09-24 15:36:02 -0700

    Force builds to use latest MSBuild & C# versions

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /nodeReuse:false /fileLogger /fileLoggerParameters:LogFile=MSBuildLog.txt
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" build.proj /nodeReuse:false /fileLogger /fileLoggerParameters:LogFile=MSBuildLog.txt
