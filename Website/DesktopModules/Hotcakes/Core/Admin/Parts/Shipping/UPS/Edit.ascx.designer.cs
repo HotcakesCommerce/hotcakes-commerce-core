a3ba5a043ac2d0ed2e4be0c4753c29dc542697b8
Liban Velázquez Silva <libanlsilva@gmail.com>
2024-10-03 11:59:08 -0400

    Update Edit.ascx.designer.cs

--- Website/DesktopModules/Hotcakes/Core/Admin/Parts/Shipping/UPS/Edit.ascx.designer.cs
+++ Website/DesktopModules/Hotcakes/Core/Admin/Parts/Shipping/UPS/Edit.ascx.designer.cs
@@ -213,6 +213,15 @@ namespace Hotcakes.Modules.Core.Admin.Parts.Shipping.UPS
         protected global::System.Web.UI.WebControls.CheckBox chkDiagnostics;
 
         /// <summary>
+        /// chkTesting control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkTesting;
+
+        /// <summary>
         /// btnSave control.
         /// </summary>
         /// <remarks>
