8833760e7a975c88afb3b3840832008b847a2b40
Will Strohl <me@willstrohl.com>
2025-03-05 18:37:32 -0800

    Updated remaining copyrights

--- Libraries/Hotcakes.Commerce.NoCms/Properties/AssemblyInfo.cs
+++ Libraries/Hotcakes.Commerce.NoCms/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hotcakes.Commerce.NoCms")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Upendo Ventures, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
